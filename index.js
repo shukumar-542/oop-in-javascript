5e690e2e4e76fb8e2c973e071a1ff063dca27986
shukumar-542 <shukumar542@gmail.com>
2023-07-31 10:52:52 +0600

    create class and constructor

---
+++ index.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+const person1 = new Person('shukumar', 'shukumar@gmail.com');
+console.log(person1);
